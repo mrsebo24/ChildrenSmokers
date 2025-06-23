dc59348ef086f99a22369b6446a334d6965c3b71
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-23 17:03:11 +0200

    created public method getSexWithWorseAverageFev and private getAverageFev

--- src/main/java/pl/homework/service/ChildAnalysis.java
+++ src/main/java/pl/homework/service/ChildAnalysis.java
@@ -27,7 +27,7 @@ public class ChildAnalysis {
     }
 
     //1. Drukującą informację o najstarszym i najmłodszym dziecku biorącym udział w badaniu (może być void i wydruk do konsoli)
-    public Optional<YoungestAndOlderChild> printOlderAndYoungestChildren(Sex sex){
+    public Optional<YoungestAndOlderChild> getOlderAndYoungestChildren(Sex sex){
         if (children.isEmpty()) return Optional.empty();
 
         List<Child> childrenOfOneSex = getChildrenOfOneSex(sex);
@@ -38,6 +38,28 @@ public class ChildAnalysis {
         if (olderChild.isEmpty() && youngestChild.isEmpty()) return Optional.empty();
         return Optional.of(new YoungestAndOlderChild(youngestChild.get(), olderChild.get()));
     }
+
+    //2. Zwracającą płeć, która ma gorsze średnie wyniki FEV.
+    public Optional<Sex> getSexWithWorseAverageFev(){
+        if (children.isEmpty()) return Optional.empty();
+
+        double female = getAverageFev(Sex.FEMALE);
+        double male = getAverageFev(Sex.MALE);
+
+        if (female > male){
+            return Optional.of(Sex.MALE);
+        }else return Optional.of(Sex.FEMALE);
+    }
+
+    private double getAverageFev(Sex sex) {
+        List<Child> childrenOfOneSex = getChildrenOfOneSex(sex);
+        double sum = 0;
+        for (Child child : children) {
+            sum += child.getFev();
+        }
+        return sum / childrenOfOneSex.size();
+    }
+
 
     private List<Child> getChildrenOfOneSex(Sex sex){
         List<Child> childrenOfOneSex = new ArrayList<>();
