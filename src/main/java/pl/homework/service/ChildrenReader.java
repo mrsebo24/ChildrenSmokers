5a57f18c92f9519b411948bb0b20427dec0d238e
mrsebo24 <sebastian.wiki@wp.pl>
2025-06-27 18:24:13 +0200

    updated few changes method names

--- src/main/java/pl/homework/service/ChildrenReader.java
+++ src/main/java/pl/homework/service/ChildrenReader.java
@@ -18,35 +18,26 @@ public class ChildrenReader {
             if (br.ready()) br.readLine();
             String line = null;
             while ((line = br.readLine()) != null){
-                children.add(getChildren(line));
+                children.add(createChildFromString(line));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }return children;
     }
 
-    private static Child getChildren(String line) {
+    private static Child createChildFromString(String line) {
         String[] split = line.split(",");
         int seqnbr = Integer.parseInt(split[0]);
         int subjid = Integer.parseInt(split[1]);
         int age = Integer.parseInt(split[2]);
         double fev = Double.parseDouble(split[3]);
         double height = Double.parseDouble(split[4]);
-        Sex sex = checkSex(Integer.parseInt(split[5]));
-        Smoke smoke = isSmoker(Integer.parseInt(split[6]));
+
+        Sex[] sexValues = Sex.values();
+        Smoke[] smokeValues = Smoke.values();
+
+        Sex sex = sexValues[(Integer.parseInt(split[5]) - 1)];
+        Smoke smoke = smokeValues[(Integer.parseInt(split[6]) - 1)];
         return new Child(seqnbr, subjid, age, fev, height, sex, smoke);
     }
-
-    private static Sex checkSex(int number){
-        if (number == 1){
-            return Sex.MALE;
-        }else return Sex.FEMALE;
-    }
-
-    private static Smoke isSmoker(int number){
-        if (number == 1){
-            return Smoke.YES;
-        }else return Smoke.NO;
-    }
-
 }
